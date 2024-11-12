480192f34665c13f9b717e68b96d90fee0490609
Jonathan Ekberg <jodrim@gmail.com>
2024-11-12 10:50:52 +0100

    Adding emails to rabbit queue

--- apps/website/src/lib/mq.ts
+++ apps/website/src/lib/mq.ts
@@ -1,1 +1,41 @@
-import { Channel } from "amqplib"
+import * as amqp from "amqplib"
+
+const EMAIL_QUEUE = "email_queue"
+const EMAIL_EXCHANGE = "email"
+const EMAIL_ROUTING_KEY = "email_route"
+
+const conn = await amqp.connect("amqp://localhost:5672")
+let channel: amqp.Channel | undefined
+
+async function getChannel() {
+  if (!channel) {
+    channel = await conn.createChannel()
+  }
+
+  await Promise.all([
+    channel.assertExchange(EMAIL_EXCHANGE, "direct"),
+    new Promise<void>(async res => {
+      if (channel) {
+        await channel.assertQueue(EMAIL_QUEUE)
+        await channel.bindQueue(EMAIL_QUEUE, EMAIL_EXCHANGE, EMAIL_ROUTING_KEY)
+      }
+      res()
+    }),
+  ])
+  return channel
+}
+
+export async function sendEmail(to: string, body: string) {
+  return (await getChannel()).publish(
+    EMAIL_EXCHANGE,
+    EMAIL_ROUTING_KEY,
+    Buffer.from(
+      JSON.stringify({
+        type: "email",
+        version: 1,
+        data: { to, body },
+      }),
+    ),
+    { headers: { "Content-Type": "application/json" } },
+  )
+}
--- apps/website/src/server/trpc/routers/auth.ts
+++ apps/website/src/server/trpc/routers/auth.ts
@@ -4,8 +4,27 @@ import { TRPCError } from "@trpc/server"
 import { hashPassword, verifyPassword } from "@/server/password"
 import { loginSchema, registerSchema } from "@/lib/validation/auth"
 import { unAuthedProcedure } from "../procedures"
+import { z } from "zod"
+import { sendEmail } from "@/lib/mq"
 
 export const authRouter = createTRPCRouter({
+  sendTwoFactorEmail: unAuthedProcedure
+    .input(
+      z.object({
+        to: z.string().email(),
+        body: z.string().max(8096),
+      }),
+    )
+    .mutation(async function ({ input }) {
+      try {
+        return await sendEmail(input.to, input.body)
+      } catch (e) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to send two factor email",
+        })
+      }
+    }),
   register: unAuthedProcedure.input(registerSchema).mutation(async function ({
     ctx,
     input,
