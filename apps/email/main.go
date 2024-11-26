6fec36c08938d4049fc97e69b36feebe085b6114
Jonathan Ekberg <jodrim@gmail.com>
2024-11-26 12:43:12 +0100

    Add rabbit connection retries
    Co-authored-by: Johan-su <Johan-su@users.noreply.github.com>
    Co-authored-by: Simon <SimonRuskola@users.noreply.github.com>

--- apps/email/main.go
+++ apps/email/main.go
@@ -1,40 +1,33 @@
 package main
 
 import (
-	"log"
-	"encoding/json"
-	"github.com/wagslane/go-rabbitmq"
-	"github.com/go-gomail/gomail"
 	"crypto/tls"
+	"encoding/json"
+	"log"
 	"os"
 	"strconv"
-)
+	"time"
 
+	"github.com/go-gomail/gomail"
+	"github.com/wagslane/go-rabbitmq"
+)
 
 const (
-	EmailRoute = "email_route"
+	EmailRoute    = "email_route"
 	EmailExchange = "email"
-	EmailQueue = "email_queue"
+	EmailQueue    = "email_queue"
 )
 
-
-
-
-
 type JsonEmailData struct {
-	To string `json:"to"`
+	To   string `json:"to"`
 	Body string `json:"body"`
 }
 
 type JsonEmail struct {
-	Type string `json:"type"`
-	Version int `json:"version"`
-	Data JsonEmailData `json:"data"`
+	Type    string        `json:"type"`
+	Version int           `json:"version"`
+	Data    JsonEmailData `json:"data"`
 }
-
-
-
-
 
 func consumer(conn *rabbitmq.Conn, d *gomail.Dialer) {
 	consumer, err := rabbitmq.NewConsumer(
@@ -48,7 +41,7 @@ func consumer(conn *rabbitmq.Conn, d *gomail.Dialer) {
 		log.Fatal(err)
 	}
 	defer consumer.Close()
-		
+
 	for {
 		err = consumer.Run(func(delivery rabbitmq.Delivery) rabbitmq.Action {
 
@@ -62,10 +55,10 @@ func consumer(conn *rabbitmq.Conn, d *gomail.Dialer) {
 			} else {
 				log.Printf("Version %d not supported", jemail.Version)
 			}
-			
-			err = sendVerifyEmail(d, jemail.Data.To, jemail.Data.Body);
+
+			err = sendVerifyEmail(d, jemail.Data.To, jemail.Data.Body)
 			if err != nil {
-				log.Printf("Failed to send email because of %v", err);
+				log.Printf("Failed to send email because of %v", err)
 				return rabbitmq.NackRequeue
 			}
 
@@ -80,7 +73,6 @@ func consumer(conn *rabbitmq.Conn, d *gomail.Dialer) {
 	}
 }
 
-
 func sendVerifyEmail(d *gomail.Dialer, to_address string, body string) error {
 	m := gomail.NewMessage()
 
@@ -90,7 +82,7 @@ func sendVerifyEmail(d *gomail.Dialer, to_address string, body string) error {
 
 	m.SetBody("text/html", body)
 
-	err := d.DialAndSend(m)	
+	err := d.DialAndSend(m)
 	return err
 }
 
@@ -110,25 +102,30 @@ func main() {
 	smtpUser := os.Getenv("SMTP_USER")
 	smtpPass := os.Getenv("SMTP_PASS")
 
-	conn, err := rabbitmq.NewConn(
-		rabbitmqHost,
-		rabbitmq.WithConnectionOptionsLogging,
-	)
-	if err != nil {
-		log.Fatal(err)
+	var conn *rabbitmq.Conn
+	var err error
+	for {
+		conn, err = rabbitmq.NewConn(
+			rabbitmqHost,
+			rabbitmq.WithConnectionOptionsLogging,
+		)
+		if err != nil {
+			log.Printf("%v", err)
+		} else {
+			break
+		}
+		time.Sleep(1 * time.Second)
 	}
 	defer conn.Close()
-	
+
 	d := gomail.NewDialer(smtpHost, smtpPort, smtpUser, smtpPass)
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
-	
-	
+
 	consumer(conn, d)
-	
+
 	// err := sendVerifyEmail(d, "test@gmail.com", "test", "https://www.getavirus.com")
 	// if err != nil {
 	// 	log.Fatalf("err = %v\n", err)
 	// }
-
 
 }
