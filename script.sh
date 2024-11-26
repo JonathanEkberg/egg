c1118e4714bb576832bd355a11ba79ab03b54284
Jonathan Ekberg <jodrim@gmail.com>
2024-11-26 12:20:06 +0100

    Actually works
    Co-authored-by: Johan-su <Johan-su@users.noreply.github.com>
    Co-authored-by: Simon <SimonRuskola@users.noreply.github.com>

--- script.sh
+++ script.sh
@@ -10,7 +10,8 @@ cd packages/database
 
 # Check if .env file exists
 if [ ! -f .env ]; then
-  echo "DATABASE_URL=postgres://root:egg@localhost:5432/egg?schema=public&sslmode=disable" > .env
+  rm .env
+  echo "DATABASE_URL=postgres://egg:egg@localhost:5432/egg?schema=public&sslmode=disable" > .env
 fi
 
 #pnpm install
