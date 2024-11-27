36fd4f9196fc46aa432e713361b987285033d9db
Jonathan Ekberg <jodrim@gmail.com>
2024-11-27 11:45:45 +0100

    Update cart count on purchase
    Co-authored-by: Simon <SimonRuskola@users.noreply.github.com>
    Co-authored-by: Johan-su <Johan-su@users.noreply.github.com>

--- apps/website/src/components/Cart/CartPurchaseButton.tsx
+++ apps/website/src/components/Cart/CartPurchaseButton.tsx
@@ -10,11 +10,13 @@ interface CartPurchaseButtonProps {
 
 export function CartPurchaseButton({ hasItems }: CartPurchaseButtonProps) {
   const router = useRouter()
+  const utils = trpc.useUtils()
   const purchase = trpc.cart.purchase.useMutation({
     onError(error, variables, context) {
       toast.error(error.message)
     },
     onSuccess() {
+      utils.cart.getMyCount.invalidate()
       router.push("/orders")
     },
   })
